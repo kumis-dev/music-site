a6e66a6ea5cb80357d5e1a17ce36c0a12f48ea79
Ivan <kumis55555@yandex.ru>
2026-02-19 20:33:42 +0300

    fix cover playlist load

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const AUDIO_BASE = 'https://music-site-files.s3.cloud.ru/audios/';
 const COVER_BASE = 'https://music-site-files.s3.cloud.ru/covers/';
+const PLAYLIST_COVER_BASE = 'https://music-site-files.s3.cloud.ru/cover-playlists/';
 
 const suctionSongs = [
   'Duality',
@@ -49,4 +50,83 @@ const kamishinVibeSongs = [
   'Nevebalo, Anton Tokar - Poka',
   'Nevebalo - Bridges',
   '25_17 - Room'
+];
+
+const playlists = [
+  {
+    className: 'suction-playlist',
+    name: 'Suction',
+    cover: 'suction-cover.png',
+    songs: suctionSongs,
+    tracks: [
+      'Slipopi – Duality',
+      'Metallica – Enter Sadman',
+      'Slipknot – Psychosocial',
+      'Ado – Useewa',
+      'Ado – Gira Gira',
+      'Linkin Park – Somewhere I Belong',
+      'Linkin Park – Faint',
+      'Three Days Grace - Never Too Late',
+      'Three Days Grace - Animal I Have Become',
+      'Linkin Park - Given Up'
+    ]
+  },
+  {
+    className: 'lofi-playlist',
+    name: 'Lofi',
+    cover: 'lofi-cover.png',
+    songs: loFiSongs,
+    tracks: ['Lofi Sleep v1']
+  },
+  {
+    className: 'anime-playlist',
+    name: 'Anime-openings',
+    cover: 'anime-cover.png',
+    songs: animeSongs,
+    tracks: ['Chainsawman - Kick Back']
+  },
+  {
+    className: 'jrock-playlist',
+    name: 'J-rock',
+    cover: 'jrock-cover.png',
+    songs: jRockSongs,
+    tracks: [
+      'Ado - Useewa',
+      'Ado - Gira Gira',
+      'Ado - Show',
+      'YOASOBI - Bakemono',
+      'Ado - Elf'
+    ]
+  },
+  {
+    className: 'rock-gay-boys-playlist',
+    name: 'Gay Boys Playlist',
+    cover: 'gayBoysPriors.png',
+    songs: rockSongsForGayBoys,
+    tracks: [
+      'Slipopi - Vendetta',
+      'Slipopi - Gehenna',
+      'Slipopi - Sufur',
+      'Vi Key - diss ms supchik',
+      'Slipopi - Wait and Bleed',
+      'Three Days Grace - Never Too Late',
+      'Three Days Grace - Animal I Have Become',
+      'Linkin Park - Given Up'
+    ]
+  },
+  {
+    className: 'kamishin-vibe-playlist',
+    name: 'Kamishin Vibe',
+    cover: 'kamishin-pop-vibe.png',
+    songs: kamishinVibeSongs,
+    tracks: [
+      'Atl - Youth',
+      'Atl - Dance',
+      'Evgeniy Trofimov - pizda',
+      'Evgeniy Trofimov - 9.11',
+      'Nevebalo, Anton Tokar - Poka',
+      'Nevebalo - Bridges',
+      '25_17 - Room'
+    ]
+  }
 ];
--- music-playlist.js
+++ music-playlist.js
@@ -1,80 +1,58 @@
-const trackElements = document.querySelectorAll('.playlist-track-title');
+const container = document.querySelector('.playlists-cards');
 
-const suctionPlaylist = document.querySelector('.suction-playlist');
-const suctionCover = document.querySelector('.suction-playlist .cover-playlist'); // обложка для открытия/скрытия треков
-const suctionHide = document.querySelector('.suction-playlist .hidden');
-const suctionName = document.querySelector('.suction-playlist .playlist-name');
-const suctionSelector = '.suction-playlist .playlist-track-title';
+playlists.forEach(playlist => {
+    // создаем карточку плейлиста
+    const card = document.createElement('div');
+    card.className = `playlist-card ${playlist.className}`;
 
-const lofiPlaylist = document.querySelector('.lofi-playlist');
-const lofiCover = document.querySelector('.lofi-playlist .cover-playlist');
-const lofiHide = document.querySelector('.lofi-playlist .hidden');
-const lofiName = document.querySelector('.lofi-playlist .playlist-name');
-const lofiSelector = '.lofi-playlist .playlist-track-title';
+     // обложка
+    const img = document.createElement('img');
+    img.className = 'cover-playlist';
+    img.src = `${PLAYLIST_COVER_BASE}${playlist.cover}`;
 
-const animePlaylist = document.querySelector('.anime-playlist');
-const animeCover = document.querySelector('.anime-playlist .cover-playlist');
-const animeHide = document.querySelector('.anime-playlist .hidden');
-const animeName = document.querySelector('.anime-playlist .playlist-name');
-const animeSelector = '.anime-playlist .playlist-track-title';
+    // скрытый блок с треками
+    const hidden = document.createElement('div');
+    hidden.className = 'hidden';
 
-const jRockPlaylist = document.querySelector('.jrock-playlist');
-const jRockCover = document.querySelector('.jrock-playlist .cover-playlist');
-const jRockHide = document.querySelector('.jrock-playlist .hidden');
-const jRockName = document.querySelector('.jrock-playlist .playlist-name');
-const jRockSelector = '.jrock-playlist .playlist-track-title';
+    // название плейлиста
+    const h2 = document.createElement('h2');
+    h2.className = 'playlist-name hidden';
+    h2.textContent = playlist.name;
 
-const rockGayBoysPlaylist = document.querySelector('.rock-gay-boys-playlist');
-const rockGayBoysCover = document.querySelector('.rock-gay-boys-playlist .cover-playlist');
-const rockGayBoysHide = document.querySelector('.rock-gay-boys-playlist .hidden');
-const rockGayBoysName = document.querySelector('.rock-gay-boys-playlist .playlist-name');
-const rockGayBoysSelector = '.rock-gay-boys-playlist .playlist-track-title';
+    // список треков
+    const ol = document.createElement('ol');
+    ol.className = 'playlist-titles';
 
-const kamishinVibePlaylist = document.querySelector('.kamishin-vibe-playlist');
-const kamishinVibeCover = document.querySelector('.kamishin-vibe-playlist .cover-playlist');
-const kamishinVibeHide = document.querySelector('.kamishin-vibe-playlist .hidden');
-const kamishinVibeName = document.querySelector('.kamishin-vibe-playlist .playlist-name');
-const kamishinVibeSelector = '.kamishin-vibe-playlist .playlist-track-title';
+    playlist.tracks.forEach((track, index) => {
+        const li = document.createElement('li'); // li - element of ol list
+        li.className = 'playlist-track-title'; // имя класса li
+        li.textContent = track; // то что внутри элемента li списка ol
 
-function playChoicePlaylist(selector, namePlaylist) {
-    document.querySelectorAll(selector).forEach((element, index) => {
-        element.addEventListener('click', () => {
+        // клик по треку - запуск в плеере
+        li.addEventListener('click', () => {
             const playerFrame = window.parent.document.getElementById('playerFrame');
-            if (playerFrame) {
-                const playerWindow = playerFrame.contentWindow;
-                playerWindow.loadPlaylist(namePlaylist, index);
-            }
+            if (playerFrame)
+                playerFrame.contentWindow.loadPlaylist(playlist.songs, index);
+        });
+
+        ol.appendChild(li);
+    });
+
+    // собираем карточку
+    hidden.appendChild(h2);
+    hidden.appendChild(ol);
+    card.appendChild(img);
+    card.appendChild(hidden);
+    container.appendChild(card);
+
+    // клик по обложке — раскрыть/скрыть треки
+    img.addEventListener('click', () => {
+        hidden.classList.toggle('hidden');
+        h1.classList.toggle('hidden');
+
+        document.querySelectorAll('.playlists-cards > div').forEach(p => {
+            if (p !== card) 
+                p.classList.toggle('hidden');
         });
     });
-}
-
-// currentPlaylist - наш плейлист, его скрывать не надо
-function openHidePlaylist(coverPlaylist, listPlaylist, namePlaylist, currentPlaylist) {
-    coverPlaylist.addEventListener('click', () => {
-        // переключаем видимость треков плейлиста
-        listPlaylist.classList.toggle('hidden');
-        namePlaylist.classList.toggle('hidden');
-        
-        // теперь нужно реализовать так, чтобы и другие плейлисты смогли бы скрыться
-        document.querySelectorAll('.playlists-cards > div').forEach(playlist => {
-            if (playlist !== currentPlaylist) {
-                playlist.classList.toggle('hidden');
-            }
-        });
-    });
-}
-
-// Инициализация
-playChoicePlaylist(suctionSelector, suctionSongs);
-playChoicePlaylist(lofiSelector, loFiSongs);
-playChoicePlaylist(animeSelector, animeSongs);
-playChoicePlaylist(rockGayBoysSelector, rockSongsForGayBoys);
-playChoicePlaylist(jRockSelector, jRockSongs);
-playChoicePlaylist(kamishinVibeSelector, kamishinVibeSongs);
-
-openHidePlaylist(suctionCover, suctionHide, suctionName, suctionPlaylist);
-openHidePlaylist(lofiCover, lofiHide, lofiName, lofiPlaylist);
-openHidePlaylist(animeCover, animeHide, animeName, animePlaylist);
-openHidePlaylist(rockGayBoysCover, rockGayBoysHide, rockGayBoysName, rockGayBoysPlaylist);
-openHidePlaylist(jRockCover, jRockHide, jRockName, jRockPlaylist);
-openHidePlaylist(kamishinVibeCover, kamishinVibeHide, kamishinVibeName, kamishinVibePlaylist);
+})
